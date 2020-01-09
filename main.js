a562e6a66f1f5a933480d17ad3d742cd2b974646
Mobolanle Adebesin <bolaadebesin@gmail.com>
2020-01-08 21:43:58 -0500

    Refactor main.js
    - Remove div elements from index.html
    - Create div  elements using JS
    
    Refactor code to create elements using JS rather than hardcoding each div element. The goal is to make code more scalable and modular.

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 eventListeners = (function() {
   let greeting = document.getElementsByClassName("greeting");
-  let greetingContainer = Array.from(
-    document.getElementsByClassName("greetingContainer")
-  );
+  let containerNum = 5;
+  let greetingContainer;
   for (let i = 0; i < greeting.length; i++) {
     greeting[i].addEventListener("click", () => {
-      if (greeting[i].id === "hello") {
+      if (greetingContainer) {
         greetingContainer.map(div => {
-          div.innerText = "Hello";
-          div.classList.remove("hidden");
-        });
-      } else {
-        greetingContainer.map(div => {
-          div.innerText = "Goodbye";
-          div.classList.remove("hidden");
+          div.remove();
         });
       }
+      if (greeting[i].id === "hello") {
+        for (let j = 0; j < containerNum; j++) {
+          newDiv = document.createElement("div");
+          newDiv.innerHTML = "Hello";
+          newDiv.setAttribute("class", "greetingContainer");
+          document.body.appendChild(newDiv);
+        }
+      } else {
+        for (let j = 0; j < containerNum; j++) {
+          newDiv = document.createElement("div");
+          newDiv.innerHTML = "Goodbye";
+          newDiv.setAttribute("class", "greetingContainer");
+          document.body.appendChild(newDiv);
+        }
+      }
+      greetingContainer = Array.from(
+        document.getElementsByClassName("greetingContainer")
+      );
     });
   }
 })();
