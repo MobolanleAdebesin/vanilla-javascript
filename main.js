429bea6cc70f35ca6e4ccda5d7d9e38b47649c08
Mobolanle Adebesin <bolaadebesin@gmail.com>
2020-01-08 19:51:06 -0500

    Add event listener to buttons

---
+++ main.js
@@ -0,0 +1,17 @@
+let greeting = document.getElementsByClassName("greeting");
+let greetingContainer = document.getElementsByClassName("greetingContainer");
+greetingContainer = Array.from(greetingContainer);
+
+for (let i = 0; i < greeting.length; i++) {
+  greeting[i].addEventListener("click", () => {
+    if (greeting[i].id === "hello") {
+      greetingContainer.map(div => {
+        div.innerText = "Hello";
+      });
+    } else {
+      greetingContainer.map(div => {
+        div.innerText = "Goodbye";
+      });
+    }
+  });
+}
