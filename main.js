5fcdf2b252038d47d58733e376c7daa06663301b
Mobolanle Adebesin <bolaadebesin@gmail.com>
2020-01-08 20:48:28 -0500

    Add Module Pattern
    - Remove global variables by wrapping code in a function

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-let greeting = document.getElementsByClassName("greeting");
-let greetingContainer = document.getElementsByClassName("greetingContainer");
-greetingContainer = Array.from(greetingContainer);
-
-for (let i = 0; i < greeting.length; i++) {
-  greeting[i].addEventListener("click", () => {
-    if (greeting[i].id === "hello") {
-      greetingContainer.map(div => {
-        div.innerText = "Hello";
-        div.classList.remove("hidden");
-      });
-    } else {
-      greetingContainer.map(div => {
-        div.innerText = "Goodbye";
-        div.classList.remove("hidden");
-      });
-    }
-  });
-}
+eventListeners = (function() {
+  let greeting = document.getElementsByClassName("greeting");
+  let greetingContainer = Array.from(
+    document.getElementsByClassName("greetingContainer")
+  );
+  for (let i = 0; i < greeting.length; i++) {
+    greeting[i].addEventListener("click", () => {
+      if (greeting[i].id === "hello") {
+        greetingContainer.map(div => {
+          div.innerText = "Hello";
+          div.classList.remove("hidden");
+        });
+      } else {
+        greetingContainer.map(div => {
+          div.innerText = "Goodbye";
+          div.classList.remove("hidden");
+        });
+      }
+    });
+  }
+})();
